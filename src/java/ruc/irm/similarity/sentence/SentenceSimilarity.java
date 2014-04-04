090b74048bcd6dcacd6a166706c11fa7bb0d578d
Summer <xiat@ruc.edu.cn>
2014-04-04 11:18:21 -0400

    remove old source code

--- src/java/ruc/irm/similarity/sentence/SentenceSimilarity.java
+++
@@ -1,7 +0,0 @@
-package ruc.irm.similarity.sentence;
-
-import ruc.irm.similarity.Similaritable;
-
-public interface SentenceSimilarity extends Similaritable {
-
-}
--- src/java/ruc/irm/similarity/word/WordSimilarity.java
+++
@@ -1,7 +0,0 @@
-package ruc.irm.similarity.word;
-
-import ruc.irm.similarity.Similaritable;
-
-public interface WordSimilarity extends Similaritable {
-
-}
